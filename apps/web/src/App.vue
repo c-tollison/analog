ff76579e1139c61bafdad86401c0e257dff82149
c-tollison <tollisoncarson@gmail.com>
2026-09-06 11:20:27 -0400

    Shadcn mocha theme, add some components for testing

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import Button from './components/shadcn-components/button/Button.vue';
+import ModeToggle from './components/ModeToggle.vue';
+import { Button } from './components/shadcn-components/button';
 
 const helloWorld = 'Hello world';
 </script>
 
 <template>
-    <main>
-        <Button>{{helloWorld}}</Button>
+    <main class="flex min-h-svh flex-col items-center justify-center gap-4">
+        <Button>{{ helloWorld }}</Button>
+        <ModeToggle />
     </main>
 </template>
