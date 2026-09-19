e8cee0471a7356c88bad6cdff22c22348e51ae99
c-tollison <tollisoncarson@gmail.com>
2026-09-19 10:39:56 -0400

    Initialize better auth instance

---
+++ apps/api/src/lib/auth.ts
@@ -0,0 +1,35 @@
+import type { Database } from '@analog/db';
+import { Stage } from '@analog/types';
+
+import type { Config } from './config.js';
+import type { Logger } from './logger.js';
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+
+export type Auth = ReturnType<typeof CreateAuthInstance>;
+
+export function CreateAuthInstance(
+    config: Config,
+    logger: Logger,
+    db: Database
+) {
+    const auth = betterAuth({
+        database: drizzleAdapter(db, {
+            provider: 'pg',
+        }),
+        secret: config.auth.secret,
+        baseURL: config.appUrl,
+        trustedOrigins: config.cors.origins,
+        logger: {
+            level: config.stage === Stage.Local ? 'debug' : 'warn',
+            log: (level, message, ...args) => logger[level]({ args }, message),
+        },
+    });
+
+    logger.info(
+        { baseUrl: config.appUrl, trustedOrigins: config.cors.origins },
+        'auth initialized'
+    );
+
+    return auth;
+}
