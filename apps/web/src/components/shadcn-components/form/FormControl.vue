089c63a82e5d04447b0efaa7d2900cf0412a2848
c-tollison <tollisoncarson@gmail.com>
2026-09-20 21:15:21 -0400

    Shadcn components

---
+++ apps/web/src/components/shadcn-components/form/FormControl.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { useFormField } from './useFormField';
+import { Slot } from 'reka-ui';
+
+const { error, formItemId, formDescriptionId, formMessageId } = useFormField();
+</script>
+
+<template>
+    <Slot
+        :id="formItemId"
+        data-slot="form-control"
+        :aria-describedby="!error ? `${formDescriptionId}` : `${formDescriptionId} ${formMessageId}`"
+        :aria-invalid="!!error"
+    >
+        <slot />
+    </Slot>
+</template>
