4bcd288697b3a23982f1918c17bc33bd36eda675
c-tollison <tollisoncarson@gmail.com>
2026-09-21 12:23:24 -0400

    OTP shadcn components

---
+++ apps/web/src/components/shadcn-components/input-otp/InputOTPGroup.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils';
+
+import { reactiveOmit } from '@vueuse/core';
+import { useForwardProps } from 'reka-ui';
+import type { HTMLAttributes } from 'vue';
+
+const props = defineProps<{ class?: HTMLAttributes['class'] }>();
+
+const delegatedProps = reactiveOmit(props, 'class');
+
+const forwarded = useForwardProps(delegatedProps);
+</script>
+
+<template>
+    <div
+        data-slot="input-otp-group"
+        v-bind="forwarded"
+        :class="cn('has-aria-invalid:ring-destructive/20 dark:has-aria-invalid:ring-destructive/40 has-aria-invalid:border-destructive rounded-md has-aria-invalid:ring-2 flex items-center', props.class)"
+    >
+        <slot />
+    </div>
+</template>
